674adfb3baf1dfdac87f89d0ed8cb1342dc9ccd1
HariOm-Coder-Ambadnya <rishipawar0101@gmail.com>
2026-07-08 23:36:01 +0530

    added

---
+++ src/One_Oops/Main.java
@@ -0,0 +1,37 @@
+package One_Oops;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student Omkar = new Student();
+        System.out.println(Omkar.name);
+        Omkar.greeting();
+        Student Anuriddha = new Student(8,"Sam",12.5f);
+        System.out.println(Anuriddha.rno);
+
+    }
+}
+
+class Student {
+
+    int rno;
+    String name;
+    float marks = 90;
+
+
+    void greeting(){
+        System.out.println("hii " + name);
+    }
+
+    Student(){
+        this.rno = 12;
+        this.name = "Rishi";
+        this.marks = 88.5f;
+    }
+
+    Student(int rollnumber, String name, float mark){
+        this.rno = rollnumber;
+        this.name = name;
+        this.marks = mark;
+    }
+}
