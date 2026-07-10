05187169411f2743cc0ae8f61861952ec8d33f7e
HariOm-Coder-Ambadnya <rishipawar0101@gmail.com>
2026-07-10 10:27:05 +0530

    completed polymorphism

---
+++ src/polymorphism/Main.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Shapes circle = new Circle();
+        Shapes sqr = new square();
+
+        circle.area();
+    }
+}
---
+++ src/polymorphism/square.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class square extends  Shapes{
+
+    @Override
+    void area(){
+        System.out.println("square");
+    }
+}
